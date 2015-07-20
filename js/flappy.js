dbf8db74ea988bb1a28af04c1b4a4222145863f3
petrichorcode <fitzroyugorji@hotmail.co.uk>
2015-07-20 12:59:02 +0100

    Update flappy.js

--- js/flappy.js
+++ js/flappy.js
@@ -1,6 +1,6 @@
 // the Game object used by the phaser.io library
 var stateActions = { preload: preload, create: create, update: update };
-
+var score;
 // Phaser parameters:
 // - game width
 // - game height
@@ -14,17 +14,32 @@ var game = new Phaser.Game(790, 400, Phaser.AUTO, 'game', stateActions);
  */
 function preload() {
     game.load.image("backgroundImg", "../assets/flappydoge.jpg");
+    game.load.audio("score", "../assets/point.ogg");
+}
 
+function spaceHandler() {
+    game.sound.play("score");
+    console.log("played");
+}
 
+function create() {game.stage.setBackgroundColor("#9999FF");
+    game.add.text(20,20, "RAGE SIMULATOR", {font: "30px Comic Sans MS", fill: "#FFFFD9"});
+    game.input
+        .onDown
+        .add(clickHandler);
+    // set the background colour of the scene
+    game.input
+        .keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+        .onDown.add(spaceHandler);
+    alert(score)
 }
 
 /*
  * Initialises the game. This function is only called once.
  */
-function create() {game.stage.setBackgroundColor("#9999FF");
-    game.add.text(20,20, "RAGE SIMULATOR", {font: "30px Comic Sans MS", fill: "#FFFFD9"});
-
-    // set the background colour of the scene
+function clickHandler(event){
+    alert("click!");
+    game.add.sprite(500, 100, "playerImg");
 }
 
 /*
@@ -33,6 +48,6 @@ function create() {game.stage.setBackgroundColor("#9999FF");
 function update() {
 
 }
-function clickHandler(event) {
-    alert("click")
-}
+
+
+
